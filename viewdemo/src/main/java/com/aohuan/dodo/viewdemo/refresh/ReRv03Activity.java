be488321c619e5802f7fe422ddda6f86fa371039
dodo_lihao 李灏 <2954722256@qq.com>
2016-12-28 10:53:00 +0800

    add
    R....

--- viewdemo/src/main/java/com/aohuan/dodo/viewdemo/refresh/ReRv03Activity.java
+++ viewdemo/src/main/java/com/aohuan/dodo/viewdemo/refresh/ReRv03Activity.java
@@ -3,6 +3,8 @@ package com.aohuan.dodo.viewdemo.refresh;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.aohuan.dodo.viewdemo.R;
+
 public class ReRv03Activity extends AppCompatActivity {
 
     @Override
